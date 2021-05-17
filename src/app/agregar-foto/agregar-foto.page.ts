87baaa1f00b168b18660ba7e02c1f1fbb60df22c
s0lid-sn4k3 <lorenzo.deleonpch@uanl.edu.mx>
2021-05-17 01:05:11 -0500

    falta acomodar el icono de quitar por favor muchas gracias o mario llora

--- src/app/agregar-foto/agregar-foto.page.ts
+++ src/app/agregar-foto/agregar-foto.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ToastController } from '@ionic/angular';
+import { CameraService } from '../camera/camera.service';
 import { FotoService } from '../tus-fotos/foto.service';
 
 @Component({
@@ -10,21 +11,24 @@ import { FotoService } from '../tus-fotos/foto.service';
   styleUrls: ['./agregar-foto.page.scss'],
 })
 export class AgregarFotoPage implements OnInit {
+  opctions: any = { weekday: 'long', month:'long', day: 'numeric',hour:'numeric',minute:'numeric'};
+
   imgForm: FormGroup;
 
   constructor(
     private fotoService: FotoService,
     private router: Router,
     public formBuilder: FormBuilder,
-    private toast: ToastController
-  ) // private cameraService: CameraService,
+    private toast: ToastController,
+    private cameraService: CameraService
+   )
   {}
 
   Data: any[] = [];
 
   // imagenes: Foto[] = [];
   isLoading = false;
-  previsualizacion: string;
+  previsualizacion: string = 'http://www.puntogps.com/images/img-no-disponible.jpg';
 
   ngOnInit() {
     this.fotoService.dbState().subscribe((res) => {
@@ -40,20 +44,33 @@ export class AgregarFotoPage implements OnInit {
       descrip: [''],
     });
   }
-
+  fechaEsp: any;
+// horario: new Date (this.myForm.value['horario']).toLocaleDateString('es-Mx', this.opctions)
   send() {
     let fecha = new Date();
-
-    this.fotoService
-      .addFoto(
+    this.fechaEsp = new Date(fecha).toLocaleDateString('es-Mx', this.opctions);
+    this.fotoService.addFoto(
         this.imgForm.value.titulo,
         this.imgForm.value.descrip,
         this.previsualizacion,
-        fecha
+        this.fechaEsp
       )
       .then((res) => {
         console.log(res);
         // this.router.navigate(['/']);
       });
+      this.getBack();
   }
+  getBack() {
+    //con esta función vamos a la ventana de agregar receta,
+    //donde podemos subir una imágen de la receta, su nombre y su preparación
+
+    this.router.navigateByUrl('/');
+  }
+
+  async takePhoto(){
+    const photo = await this.cameraService.getPhoto()
+    this.previsualizacion = 'data:image/jpeg;base64,'+ photo.base64String;
+  }
+
 }
---
+++ src/app/camera/camera.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import {
+  Plugins, CameraResultType, Capacitor, FilesystemDirectory,
+  CameraPhoto, CameraSource
+} from '@capacitor/core';
+
+const { Camera, Filesystem, Storage } = Plugins;
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CameraService {
+  async getPhoto(){
+    const capturedPhoto = await Camera.getPhoto({
+      resultType:CameraResultType.Base64,
+      source:CameraSource.Camera,
+      quality:100
+    })
+
+    return capturedPhoto
+  }
+
+}
